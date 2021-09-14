53e0d5b95d82ff8633c90ed556ffe256ae013a19
bbtemb <63672437+bbtemb@users.noreply.github.com>
2021-09-14 15:42:41 +0500

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $name = htmlspecialchars($_POST['name']);
-  $phone  = htmlspecialchars($_POST['phone']);
-  $email  = htmlspecialchars($_POST['email']);
-
-  echo  $name, ' ', $phone,' ', $email;
-?>
